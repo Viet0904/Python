fe1560c5ba4941d16d831035deb6b147f688a1c2
Nqviet0904 <111555381+Nqviet0904@users.noreply.github.com>
2023-12-28 22:52:42 +0700

    dasdsa

--- day20/Snake_Game/main.py
+++ day20/Snake_Game/main.py
@@ -12,16 +12,23 @@ snake = Snake()
 
 screen.listen()
 game_is_on = True
-screen.onkey(snake.up,"Up",)
-screen.onkey(snake.down,"Down",)
-screen.onkey(snake.left,"Left",)
+screen.onkey(
+    snake.up,
+    "Up",
+)
+screen.onkey(
+    snake.down,
+    "Down",
+)
+screen.onkey(
+    snake.left,
+    "Left",
+)
 screen.onkey(snake.right, "Right")
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
-    
 
 
 screen.exitonclick()
